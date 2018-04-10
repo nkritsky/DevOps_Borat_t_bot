8dd00221df24db81ce0a57d426002aad3086dcea
Nick Kritsky <nick.kritsky@gmail.com>
2018-04-10 23:45:52 +0300

    enable webhooks

--- app.py
+++ app.py
@@ -87,12 +87,18 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    # if the environment variable BOT_CONFIG_service_url - register the webhook
+    if "BOT_CONFIG_service_url" in os.environ:
+        updater.start_webhook(listen='0.0.0.0',
+                      port=8080,
+                      url_path=TOKEN)
+        updater.bot.set_webhook(webhook_url=webhook_url+'/TOKEN')
+    else:
+        updater.start_polling()
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        updater.idle()
 
 
 if __name__ == '__main__':
